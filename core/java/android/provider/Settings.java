cb8a49f9e28d7496f03242a810ad145c639ccf51
Lars Greiss <kufikugel@googlemail.com>
2014-12-16 17:25:58 +0000

    add ability to disable bar color in battery saver mode (1/2)
    Change-Id: I6ef0dc31d74e146301061473b9fd68968e7d42ee
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5070,6 +5070,13 @@ public final class Settings {
         public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
 
         /**
+         * Whether to enable status and navigation bar color in battery saver mode.
+         *
+         * @hide
+         */
+        public static final String BATTERY_SAVER_MODE_COLOR = "battery_save_mode_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
