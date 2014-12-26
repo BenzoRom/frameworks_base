bf3ff1a212d1132c471646445e2fb2274e8d78b5
chunshu liu <chunshu.x.liu@sonymobile.com>
2014-12-26 11:14:20 +0900

    Turn LCD off when mirroring display to save power
    registered as service, getSystemsService() then call it to turn lcd off
    
    Test: mirror screen to TV, phone lcd is off
    
    BenzoEdit: Fixed up for Pie
    Change-Id: I690275d126afbf8ab56b608959a32343fba91f12
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -102,6 +102,8 @@ import com.android.server.power.batterysaver.BatterySaverController;
 import com.android.server.power.batterysaver.BatterySaverStateMachine;
 import com.android.server.power.batterysaver.BatterySavingStats;
 
+import com.android.internal.mirrorpowersave.LcdPowerSaveInternal;
+
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.lang.annotation.Retention;
@@ -651,6 +653,8 @@ public final class PowerManagerService extends SystemService
     private static native void nativeSendPowerHint(int hintId, int data);
     private static native void nativeSetFeature(int featureId, int data);
 
+    private LcdPowerSaveInternal mLcdPowerSaveInternal;
+
     public PowerManagerService(Context context) {
         super(context);
         mContext = context;
@@ -760,6 +764,8 @@ public final class PowerManagerService extends SystemService
             mScreenBrightnessSettingDefault = pm.getDefaultScreenBrightnessSetting();
 
             SensorManager sensorManager = new SystemSensorManager(mContext, mHandler.getLooper());
+
+            mLcdPowerSaveInternal = getLocalService(LcdPowerSaveInternal.class);
 
             // The notifier runs on the system server's main looper so as not to interfere
             // with the animations and other critical functions of the power manager.
@@ -1260,6 +1266,7 @@ public final class PowerManagerService extends SystemService
     }
 
     private void userActivityInternal(long eventTime, int event, int flags, int uid) {
+        mLcdPowerSaveInternal.userActivity(eventTime, event);
         synchronized (mLock) {
             if (userActivityNoUpdateLocked(eventTime, event, flags, uid)) {
                 updatePowerStateLocked();
@@ -2522,6 +2529,7 @@ public final class PowerManagerService extends SystemService
                 userActivityNoUpdateLocked(SystemClock.uptimeMillis(),
                         PowerManager.USER_ACTIVITY_EVENT_OTHER, 0, Process.SYSTEM_UID);
                 updatePowerStateLocked();
+                mLcdPowerSaveInternal.interceptProximityWhenLcdOn();
             }
         }
 
