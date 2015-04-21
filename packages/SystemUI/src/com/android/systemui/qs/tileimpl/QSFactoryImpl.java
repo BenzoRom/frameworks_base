43ff8a22879172bed71fcd881df3f66b716df39b
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    QS: SystemUI: Add heads up tile
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -35,6 +35,7 @@ import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
@@ -128,6 +129,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new BenzoExtrasTile(mHost);
             case "reboot":
                 return new RebootTile(mHost);
+            case "heads_up":
+                return new HeadsUpTile(mHost);
         }
 
         // Intent tiles.
