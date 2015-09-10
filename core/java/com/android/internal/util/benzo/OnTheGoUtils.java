c61b172db431990ba309d9f717708044e6e4d799
Alexander Martinz <eviscerationls@gmail.com>
2015-09-09 18:07:01 -0700

    SystemUI: On-The-Go Mode (1/2)
    jaysonedson: Ported to android12. Updated to kotlin.
    Change-Id: If3db29575cefd4a46ae387a5e3f7464e62bd3edd
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

---
+++ core/java/com/android/internal/util/benzo/OnTheGoUtils.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2014 The NamelessROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.android.internal.util.benzo;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import java.util.List;
+
+public class OnTheGoUtils {
+    /**
+     * Checks if a specific service is running.
+     *
+     * @param context     The context to retrieve the activity manager
+     * @param serviceName The name of the service
+     * @return Whether the service is running or not
+     */
+    public static boolean isServiceRunning(Context context, String serviceName) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> services = am.getRunningServices(Integer.MAX_VALUE);
+        if (services != null) {
+            for (ActivityManager.RunningServiceInfo info : services)
+                if (info.service != null)
+                    if (info.service.getClassName() != null && info.service.getClassName()
+                            .equalsIgnoreCase(serviceName)) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check if system has a camera.
+     */
+    public static boolean hasCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    /**
+     * Check if system has a front camera.
+     */
+    public static boolean hasFrontCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
+    }
+}
