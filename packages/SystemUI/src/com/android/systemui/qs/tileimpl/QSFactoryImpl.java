8f19476461a4533b622db2099ec5f72cd747ffc4
Varun Date <date.varun123@gmail.com>
2016-09-28 02:26:36 +0530

    QS: SystemUI: Add BenzoExtras Tile
    Thanks to Brett Rogers
    
    Change-Id: Ie9e5a8dd9ba675a14aceac4699b75a06b64a1ee3
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -26,6 +26,7 @@ import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AlwaysOnDisplayTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
+import com.android.systemui.qs.tiles.BenzoExtrasTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
@@ -122,6 +123,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new CPUInfoTile(mHost);
             case "usb_tether":
                 return new UsbTetherTile(mHost);
+            case "benzoextras":
+                return new BenzoExtrasTile(mHost);
         }
 
         // Intent tiles.
