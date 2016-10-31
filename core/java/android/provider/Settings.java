898c3a9aa3d563f7ec680de6377c69a75fd4a592
maxwen <max.weninger@gmail.com>
2016-10-31 23:48:50 +0100

    SystemUI: make QS columns count configurable
    And add infrastructure to add more settings and
    distribute them to QS panel
    
    PS4
    we must explicitly hide the columns submenu
    
    base: SystemUI: add column count 6
    
    now that we have hide label
    
    base: SystemUI: qs colums menu with checkboxes
    
    base: SystemUI add landscape qs columns config
    
    allow max 9 columns in landscape
    
    Change-Id: I401cca347da1a94f352a9ffcb99f1409ddc4d008
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4088,6 +4088,17 @@ public final class Settings {
         public static final String BATTERY_BAR_USE_GRADIENT_COLOR = "battery_bar_use_gradient_color";
 
         /**
+         * Number of qs columns on landscape orientation
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS_LANDSCAPE = "qs_layout_columns_landscape";
+
+        /**
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
