56194996646af6e2cc41f9ff8218320d3f87828f
Michael W <baddaemon87@gmail.com>
2016-12-23 13:33:45 +0100

    SystemUI: Recents: Allow closing empty view via tapping
    Change-Id: I7fa7e48c38db503ea152cd64dd98acbb2943f15f
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -82,6 +82,7 @@ import com.android.systemui.recents.events.activity.LaunchTaskEvent;
 import com.android.systemui.recents.events.activity.MultiWindowStateChangedEvent;
 import com.android.systemui.recents.events.activity.ShowEmptyViewEvent;
 import com.android.systemui.recents.events.activity.ShowStackActionButtonEvent;
+import com.android.systemui.recents.events.activity.ToggleRecentsEvent;
 import com.android.systemui.recents.events.component.ExpandPipEvent;
 import com.android.systemui.recents.events.ui.AllTaskViewsDismissedEvent;
 import com.android.systemui.recents.events.ui.DismissAllTaskViewsEvent;
@@ -430,6 +431,12 @@ public class RecentsView extends FrameLayout {
         if (mFloatingButton != null) {
             mFloatingButton.setVisibility(View.GONE);
         }
+        setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EventBus.getDefault().send(new ToggleRecentsEvent());
+            }
+        });
     }
 
     /**
@@ -445,6 +452,7 @@ public class RecentsView extends FrameLayout {
         if (mFloatingButton != null) {
             mFloatingButton.setVisibility(View.VISIBLE);
         }
+        setOnClickListener(null);
     }
 
     /**
@@ -690,7 +698,11 @@ public class RecentsView extends FrameLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        return mTouchHandler.onTouchEvent(ev);
+        if (mTouchHandler.onTouchEvent(ev)) {
+            return true;
+        } else {
+            return super.onTouchEvent(ev);
+        }
     }
 
     @Override
