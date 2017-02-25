baaa1b364ce021bc95e36fec1020ea79f74349c9
maxwen <max.weninger@gmail.com>
2017-02-25 17:31:05 +0100

    SystemUI: OmniJaws qs tile integration
    Change-Id: I4a00255a362298820f9f7997b3089e135358e0f6
    
    base: qs weather: be nice to people without OmniJaws
    
    if OmniJaws is not available or disabled show an informative
    Toast. Also change the default tile image to show also some sun :)
    
    Change-Id: Id1216dfc2aa5a9cf86e1b6accaf354f25c5541fd
    
    base: SystemUI: remove force weather update bits
    
    force updates have been removed from service API
    
    Change-Id: I183bb1a3990d28211d99ff024f9faf59faa6ac4b
    
    base: SystemUI: use new OmniJaws error broadcasts
    
    Change-Id: I3c7da3843186897b7c93d477ca0696a39e0ada65
    
    base: SystemUI: dont hide weather forcast data on errors
    
    only show error in header
    see https://gerrit.omnirom.org/23116 for details
    
    Change-Id: Iaa2fa7b6e0d6638b142b19149c5b647d64f3b2e0
    
    base: SystemUI: dont show OmniJaws error that prominent
    
    instead just show a small * to indicate an error. In best case
    the update has been triggered on screen on anyway and will
    be updated soon
    
    Change-Id: I31446c8339e8093e41f9fc8a2788b251bcd3dced
    
    base: SystemUI: add dedicated TextView for OmniJaws error marker
    
    dont mess with the layout in the error case
    and fix proper layout handling for loooong city names
    
    @mydongistiny: moved OmniJawsClient over to core/java/com/android/internal/util/benzo
    	so there's only need for one client instead of a new one because of the keyguard
    Change-Id: I52919421cbb550e2a662e7df65118d4b03dcebf4
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/util/benzo/OmniJawsClient.java
+++ core/java/com/android/internal/util/benzo/OmniJawsClient.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.android.keyguard;
+package com.android.internal.util.benzo;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -69,7 +69,9 @@ public class OmniJawsClient {
 
     final String[] SETTINGS_PROJECTION = new String[] {
             "enabled",
-            "units"
+            "units",
+            "provider",
+            "setup"
     };
 
     private static final String WEATHER_UPDATE = "org.omnirom.omnijaws.WEATHER_UPDATE";
@@ -89,6 +91,7 @@ public class OmniJawsClient {
         public List<DayForecast> forecasts;
         public String tempUnits;
         public String windUnits;
+        public String provider;
 
         public String toString() {
             return city + ":" + new Date(timeStamp) + ": " + windSpeed + ":" + windDirection + ":" +conditionCode + ":" + temp + ":" + humidity + ":" + condition + ":" + tempUnits + ":" + windUnits + ": " + forecasts;
@@ -188,14 +191,22 @@ public class OmniJawsClient {
         }
     }
 
-    public void updateWeather(boolean force) {
+    public void updateWeather() {
         if (isOmniJawsServiceInstalled()) {
             Intent updateIntent = new Intent(Intent.ACTION_MAIN)
                     .setClassName(SERVICE_PACKAGE, SERVICE_PACKAGE + ".WeatherService");
             updateIntent.setAction(SERVICE_PACKAGE + ".ACTION_UPDATE");
-            updateIntent.putExtra("force", force);
             mContext.startService(updateIntent);
         }
+    }
+
+    public Intent getSettingsIntent() {
+        if (isOmniJawsServiceInstalled()) {
+            Intent settings = new Intent(Intent.ACTION_MAIN)
+                    .setClassName("com.android.settings", "com.benzo.settings.Settings$LockScreenWeatherSettingsActivity");
+            return settings;
+        }
+        return null;
     }
 
     public WeatherInfo getWeatherInfo() {
@@ -338,12 +349,31 @@ public class OmniJawsClient {
 
     public void setOmniJawsEnabled(boolean value) {
         if (isOmniJawsServiceInstalled()) {
+            // check first time enablement and redirect to settings
+            // cause we need to enable gps for it
             Intent updateIntent = new Intent(Intent.ACTION_MAIN)
                     .setClassName(SERVICE_PACKAGE, SERVICE_PACKAGE + ".WeatherService");
             updateIntent.setAction(SERVICE_PACKAGE + ".ACTION_ENABLE");
             updateIntent.putExtra("enable", value);
             mContext.startService(updateIntent);
         }
+    }
+
+    public boolean isOmniJawsSetupDone() {
+        if (!isOmniJawsServiceInstalled()) {
+            return false;
+        }
+        final Cursor c = mContext.getContentResolver().query(SETTINGS_URI, SETTINGS_PROJECTION,
+                null, null, null);
+        if (c != null) {
+            int count = c.getCount();
+            if (count == 1) {
+                c.moveToPosition(0);
+                boolean setupDone = c.getInt(3) == 1;
+                return setupDone;
+            }
+        }
+        return true;
     }
 
     private void updateUnits() {
@@ -360,6 +390,7 @@ public class OmniJawsClient {
                 if (mCachedInfo != null) {
                     mCachedInfo.tempUnits = getTemperatureUnit();
                     mCachedInfo.windUnits = getWindUnit();
+                    mCachedInfo.provider = c.getString(2);
                 }
             }
         }
@@ -399,7 +430,7 @@ public class OmniJawsClient {
     }
 
     public Drawable getDefaultWeatherConditionImage() {
-        return mContext.getResources().getDrawable(R.drawable.keyguard_weather_default_on);
+        return mContext.getResources().getDrawable(com.android.internal.R.drawable.ic_qs_weather_default_on);
     }
 
     public void addObserver(OmniJawsObserver observer) {
