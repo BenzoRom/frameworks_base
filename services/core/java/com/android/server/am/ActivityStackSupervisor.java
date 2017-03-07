1d693a54737db0ac3876ce5f24d719d6089644ed
Sashidhar Ganiga <sganig@codeaurora.org>
2017-03-07 17:30:10 +0530

    Fix wakelock being held during bootup and idle scenario
    On bootup of device, if device is kept idle without unlocking the
    screen the TCXO does not happen due to "ActivityManager-Sleep"
    partial wake_lock being held.
    
    Fix releases the wakelock irrespective of the result of code flow
    in checkReadyForSleepLocked() function.
    
    Change-Id: I5a61463aa29ac5e94020111b93b7eba2ba170513
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -3173,6 +3173,9 @@ public class ActivityStackSupervisor extends ConfigurationContainer implements D
         applySleepTokensLocked(false /* applyToStacks */);
 
         checkReadyForSleepLocked(true /* allowDelay */);
+        if (mGoingToSleep.isHeld()) {
+            mGoingToSleep.release();
+        }
     }
 
     void prepareForShutdownLocked() {
@@ -3287,9 +3290,6 @@ public class ActivityStackSupervisor extends ConfigurationContainer implements D
 
         removeSleepTimeouts();
 
-        if (mGoingToSleep.isHeld()) {
-            mGoingToSleep.release();
-        }
         if (mService.mShuttingDown) {
             mService.notifyAll();
         }
