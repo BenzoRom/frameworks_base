e2b737057e88adf238717f912ff2c3dcef21ccfa
xiaomei.li <xiaomei.li@spreadtrum.com>
2017-08-16 10:35:02 +0800

    Avoid the restart process for launching provider killed by AMS
    The case is that a content provider process killed by LowmemoryKiller while the
    provider launching time. And AMS will restart it for the launching provider,and
    reuse the ProcessRecord, it may be killed by AMS#updateOomAdjLocked in the end
    of attachApplicationLocked for empty cached process.
    
    After that the process will restart again, and may be killed angian, another
    restart, another restart...
    
    So, we should give time for process to launch its provider, avoid the restart
    and kill circle.
    
    Test: monkey test for 10h without related issue
    Change-Id: If4be91faf804493f48d3d075799576e39561ea09
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -7087,6 +7087,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
         mHandler.removeMessages(PROC_START_TIMEOUT_MSG, app);
 
+        boolean didSomething = false;
         boolean normalMode = mProcessesReady || isAllowedWhileBooting(app.info);
         List<ProviderInfo> providers = normalMode ? generateApplicationProvidersLocked(app) : null;
 
@@ -7094,6 +7095,7 @@ public class ActivityManagerService extends IActivityManager.Stub
             Message msg = mHandler.obtainMessage(CONTENT_PROVIDER_PUBLISH_TIMEOUT_MSG);
             msg.obj = app;
             mHandler.sendMessageDelayed(msg, CONTENT_PROVIDER_PUBLISH_TIMEOUT);
+            didSomething = true;
         }
 
         checkTime(startTime, "attachApplicationLocked: before bindApplication");
@@ -7250,7 +7252,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         mProcessesOnHold.remove(app);
 
         boolean badApp = false;
-        boolean didSomething = false;
 
         // See if the top visible activity is waiting to run in this process...
         if (normalMode) {
