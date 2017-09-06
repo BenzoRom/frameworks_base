dfc02b333a1648be403cdda3ef40f0f34a4b49c9
bigrushdog <randall.rushing@gmail.com>
2017-09-06 20:46:51 +0200

    core: Add api to take screenshots
    Omni: extracted from DUI
    add a public call to benzoUtils
    
    Change-Id: If71944466c703a9b3fe54aea6bb21150b0a082bb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/util/benzo/benzoUtils.java
+++ core/java/com/android/internal/util/benzo/benzoUtils.java
@@ -17,12 +17,18 @@
 package com.android.internal.util.benzo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.PowerManager;
+import android.os.RemoteException;
 import android.os.SystemClock;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 
 public class benzoUtils {
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     public static boolean isWifiOnly(Context context) {
         ConnectivityManager cm = (ConnectivityManager)context.getSystemService(
@@ -34,6 +40,15 @@ public class benzoUtils {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
+        }
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
     }
 }
