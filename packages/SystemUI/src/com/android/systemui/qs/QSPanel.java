a86234184957ec9950f10a40feb1e8f0b49ed9b6
maxwen <max.weninger@gmail.com>
2017-09-21 03:29:56 +0200

    SystemUI: add back QS hide label
    if enabled label click (secondary) will be reassigned to long click
    for dual target tiles
    
    Change-Id: I440362ab38057cbfba7750d75825179bd830c4e8
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -438,11 +438,11 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         r.callback = callback;
         r.tileView.init(r.tile);
         r.tile.refreshState();
-        r.tileView.setHideExpand(mTileLayout.getNumColumns() > 4);
         mRecords.add(r);
 
         if (mTileLayout != null) {
             mTileLayout.addTile(r);
+            configureTile(r.tile, r.tileView);
         }
 
         return r;
@@ -625,8 +625,29 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         boolean updateResources();
         void updateSettings();
         int getNumColumns();
+        boolean isShowTitles();
 
         void setListening(boolean listening);
+    }
+
+    private void configureTile(QSTile t, QSTileView v) {
+        if (mTileLayout != null) {
+            v.setHideExpand(mTileLayout.getNumColumns() > 4);
+            v.setHideLabel(!mTileLayout.isShowTitles());
+            if (t.isDualTarget()) {
+                if (!mTileLayout.isShowTitles()) {
+                    v.setOnLongClickListener(view -> {
+                        t.secondaryClick();
+                        return true;
+                    });
+                } else {
+                    v.setOnLongClickListener(view -> {
+                        t.longClick();
+                        return true;
+                    });
+                }
+            }
+        }
     }
 
     public void updateSettings() {
@@ -634,8 +655,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             mTileLayout.updateSettings();
 
             for (TileRecord r : mRecords) {
-                QSTileView v = r.tileView;
-                v.setHideExpand(mTileLayout.getNumColumns() > 4);
+                configureTile(r.tile, r.tileView);
             }
         }
     }
