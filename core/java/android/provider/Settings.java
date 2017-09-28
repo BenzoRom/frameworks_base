f4ce521cf381ca61743cd7ac41d7c4abda247b5a
Danesh M <daneshm90@gmail.com>
2017-09-28 09:57:06 +0800

    SystemUI: Port forward statusbar brightness control [1/2]
    Brings back the status bar brightness control from cm-10.2/cm-11
    
    SystemUI: improve brightness slider behavior
    Mimic the brightness slider behavior in the statusbar.
    This adds logic to make the statusbar slider also work with automatic
    brightness mode enabled and it will instead adjust the temporary automatic
    brightness overrride.
    Change-Id: I78814a785decc83df94844fcc2f489c8cbec2b65
    
    SystemUI: status bar brightness: store value as int
    Non-automatic brightness value is stored as int, not float.
    Symptom: adjust the brightness in the status bar, the brightness
             slider in the notification bar will always be set to full
    Change-Id: Icc04e7da3286e9925f8b90528c1e022b33d33e9d
    
    SystemUI: fix status bar brightness slider eating events
    In some cases it would eat the event and not lose focus to the rest of
    the system.
    
    BenzoEdit: Fixed for Pie
    Change-Id: I94d532d09ea762edf7ac037043ed023caf42c401
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4754,6 +4754,12 @@ public final class Settings {
         private static final Validator BOTTOM_GESTURE_SWIPE_START_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * Whether to control brightness from status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4975,6 +4981,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BOTTOM_GESTURE_TRIGGER_TIMEOUT);
             PRIVATE_SETTINGS.add(BOTTOM_GESTURE_SWIPE_LIMIT);
             PRIVATE_SETTINGS.add(BOTTOM_GESTURE_SWIPE_START);
+            PRIVATE_SETTINGS.add(STATUS_BAR_BRIGHTNESS_CONTROL);
         }
 
         /**
