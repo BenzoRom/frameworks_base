28a196f8261c49dc516d236322aa5a01c7844c7b
wangmingming1 <wangmingming1@xiaomi.com>
2018-04-28 17:39:38 +0800

    Segments in package name must starts with letters
    keep package name check sync with installd and AAPT2
    
    Test: 1. installaltion of app with package name looks like
             com.xxx. or .com.xxx should fail
    
    Change-Id: I64b0c5b97d54fec06f92095762f22ed29592b1b2
    Signed-off-by: wangmingming1 <wangmingming1@xiaomi.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/content/pm/PackageParser.java
+++ core/java/android/content/pm/PackageParser.java
@@ -1857,6 +1857,9 @@ public class PackageParser {
             }
             return "bad character '" + c + "'";
         }
+        if (front) {
+            return "Each segment must start with a letter";
+        }
         if (requireFilename && !FileUtils.isValidExtFilename(name)) {
             return "Invalid filename";
         }
