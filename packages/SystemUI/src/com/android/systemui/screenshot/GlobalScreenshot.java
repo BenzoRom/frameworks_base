54edd73c5e2f3188281ec2acda7f5f07529ccc9c
Wang Han <wanghan1995315@gmail.com>
2018-05-21 02:00:02 +0200

    SystemUI: Dont add app name to screenshot filename when on keyguard
    Change-Id: Ic77179b61eebfaf1f68303dcba5422d1b6848263
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -28,6 +28,7 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityOptions;
 import android.app.admin.DevicePolicyManager;
+import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.Notification.BigPictureStyle;
 import android.app.NotificationManager;
@@ -156,7 +157,8 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
         mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE, imageDate);
         final PackageManager pm = context.getPackageManager();
         ActivityInfo info = benzoUtils.getRunningActivityInfo(context);
-        if (info != null) {
+        boolean onKeyguard = context.getSystemService(KeyguardManager.class).isKeyguardLocked();
+        if (info != null && !onKeyguard) {
             CharSequence appName = pm.getApplicationLabel(info.applicationInfo);
             if (appName != null) {
                 // replace all spaces and special chars with an underscore
