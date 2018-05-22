c2b8dd925f68d8e4d845af034266fb5f5dc3ea36
John Zhao <yuankuiz@qualcomm.corp-partner.google.com>
2018-05-22 11:13:31 +0800

    base: BatteryStats stat cpufreq access check
    The "/sys/devices/system/cpu/cpu%d/cpufreq/stats/time_in_state" is
    depends on the "CONFIG_CPU_FREQ_STAT=y" inside of kernel config,
    which is often be off as can save additional stat obviously.
    
    Test: toggeling the above option in kernel config and get the logcat.
    
    Change-Id: I6553f7f78d8a0f719050be7bedcd005898c9f95f
    Signed-off-by: John Zhao <yuankuiz@qualcomm.corp-partner.google.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/os/KernelCpuSpeedReader.java
+++ core/java/com/android/internal/os/KernelCpuSpeedReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2015, 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import android.os.StrictMode;
 import android.system.OsConstants;
 import android.util.Slog;
 
+import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -40,6 +41,7 @@ public class KernelCpuSpeedReader {
     private final String mProcFile;
     private final long[] mLastSpeedTimesMs;
     private final long[] mDeltaSpeedTimesMs;
+    private boolean mAvailable;
 
     // How long a CPU jiffy is in milliseconds.
     private final long mJiffyMillis;
@@ -54,6 +56,12 @@ public class KernelCpuSpeedReader {
         mDeltaSpeedTimesMs = new long[numSpeedSteps];
         long jiffyHz = Os.sysconf(OsConstants._SC_CLK_TCK);
         mJiffyMillis = 1000/jiffyHz;
+        if (new File(mProcFile).exists()) {
+            mAvailable = true;
+        } else {
+            mAvailable = false;
+            Slog.d(TAG, "Cpufreq Stats file is not found at:" + mProcFile + ".");
+        }
     }
 
     /**
@@ -89,5 +97,9 @@ public class KernelCpuSpeedReader {
             StrictMode.setThreadPolicy(policy);
         }
         return mDeltaSpeedTimesMs;
+    }
+
+    public boolean getAvailability() {
+        return mAvailable;
     }
 }
