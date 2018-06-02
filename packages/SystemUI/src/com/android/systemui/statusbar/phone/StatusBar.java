8faec6af8e730e949caef3a336ec2f7378822387
ezio84 <brabus84@gmail.com>
2018-06-02 18:31:37 +0200

    AmbientMusicTicker: skip to next track if doubletap on track indicator
    double tapping on track info or the music icon will skip to the next
    track
    
    this uses previous remote control skip track commit so will work
    even with remote playing sessions like spotify
    
    Change-Id: I60fd58ff6f597f282db367c80cca001545929310
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -6150,7 +6150,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         @Override
         public void onDoubleTap(float screenX, float screenY) {
-            if (screenX > 0 && screenY > 0 && mAmbientIndicationContainer != null 
+            /* if (screenX > 0 && screenY > 0 && mAmbientIndicationContainer != null 
                 && mAmbientIndicationContainer.getVisibility() == View.VISIBLE) {
                 mAmbientIndicationContainer.getLocationOnScreen(mTmpInt2);
                 float viewX = screenX - mTmpInt2[0];
@@ -6159,7 +6159,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                         && 0 <= viewY && viewY <= mAmbientIndicationContainer.getHeight()) {
                     dispatchDoubleTap(viewX, viewY);
                 }
-            }
+            } */
         }
 
         @Override
@@ -6235,6 +6235,23 @@ public class StatusBar extends SystemUI implements DemoMode,
                 notifyUiVisibilityChanged(mSystemUiVisibility);
             }
         }
+    }
+
+    public boolean isDoubleTapOnMusicTicker(float eventX, float eventY) {
+        if (eventX <= 0 || eventY <= 0 || mAmbientIndicationContainer == null
+                || mAmbientIndicationContainer.getVisibility() != View.VISIBLE) {
+            return false;
+        }
+        final View indication = ((AmbientIndicationContainer)mAmbientIndicationContainer).getIndication();
+        indication.getLocationOnScreen(mTmpInt2);
+        float viewX = eventX - mTmpInt2[0];
+        float viewY = eventY - mTmpInt2[1];
+        if (0 <= viewX && viewX <= indication.getWidth()
+                && 0 <= viewY && viewY <= indication.getHeight()) {
+            handleSystemKey(KeyEvent.KEYCODE_MEDIA_NEXT);
+            return true;
+        }
+        return false;
     }
 
     // Begin Extra BaseStatusBar methods.
