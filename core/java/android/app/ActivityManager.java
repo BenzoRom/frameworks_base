eb48b83982da8abf5e387decaa99a8ef040feb8c
qiushi.han <qiushi.han@spreadtrum.com>
2018-06-29 13:53:25 +0800

    Remove another unused calling for better performance
    getWindowContainerBounds will be called many times, trying to hold
    WMS lock, leading to bad performance, but no one will use this
    taskBounds.
    
    Test: make services
    Change-Id: I5d1cd162643fdc15dc1bc739abcf853ebd2dc91d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/app/ActivityManager.java
+++ core/java/android/app/ActivityManager.java
@@ -2793,7 +2793,6 @@ public class ActivityManager {
         public Rect bounds = new Rect();
         public int[] taskIds;
         public String[] taskNames;
-        public Rect[] taskBounds;
         public int[] taskUserIds;
         public ComponentName topActivity;
         public int displayId;
@@ -2816,14 +2815,6 @@ public class ActivityManager {
             dest.writeInt(bounds.bottom);
             dest.writeIntArray(taskIds);
             dest.writeStringArray(taskNames);
-            final int boundsCount = taskBounds == null ? 0 : taskBounds.length;
-            dest.writeInt(boundsCount);
-            for (int i = 0; i < boundsCount; i++) {
-                dest.writeInt(taskBounds[i].left);
-                dest.writeInt(taskBounds[i].top);
-                dest.writeInt(taskBounds[i].right);
-                dest.writeInt(taskBounds[i].bottom);
-            }
             dest.writeIntArray(taskUserIds);
             dest.writeInt(displayId);
             dest.writeInt(userId);
@@ -2843,17 +2834,6 @@ public class ActivityManager {
                     source.readInt(), source.readInt(), source.readInt(), source.readInt());
             taskIds = source.createIntArray();
             taskNames = source.createStringArray();
-            final int boundsCount = source.readInt();
-            if (boundsCount > 0) {
-                taskBounds = new Rect[boundsCount];
-                for (int i = 0; i < boundsCount; i++) {
-                    taskBounds[i] = new Rect();
-                    taskBounds[i].set(
-                            source.readInt(), source.readInt(), source.readInt(), source.readInt());
-                }
-            } else {
-                taskBounds = null;
-            }
             taskUserIds = source.createIntArray();
             displayId = source.readInt();
             userId = source.readInt();
@@ -2893,9 +2873,6 @@ public class ActivityManager {
             for (int i = 0; i < taskIds.length; ++i) {
                 sb.append(prefix); sb.append("taskId="); sb.append(taskIds[i]);
                         sb.append(": "); sb.append(taskNames[i]);
-                        if (taskBounds != null) {
-                            sb.append(" bounds="); sb.append(taskBounds[i].toShortString());
-                        }
                         sb.append(" userId=").append(taskUserIds[i]);
                         sb.append(" visible=").append(visible);
                         if (topActivity != null) {
