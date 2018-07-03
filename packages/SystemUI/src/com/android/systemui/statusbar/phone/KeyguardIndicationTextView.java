688fec885fde32127417c9570f67e819edcd0c58
mydongistiny <jaysonedson@gmail.com>
2018-07-02 18:58:04 -0700

    Revert "SystemUI: Animate switching indications on keyguard"
    This reverts commit 96ad58b34eb7d1e50fce74e55d8f3ab306953c94.
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardIndicationTextView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardIndicationTextView.java
@@ -20,17 +20,12 @@ import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.Interpolator;
 import android.widget.TextView;
-
-import com.android.systemui.Interpolators;
 
 /**
  * A view to show hints on Keyguard ("Swipe up to unlock", "Tap again to open").
  */
 public class KeyguardIndicationTextView extends TextView {
-
-    private static final long ANIMATION_DURATION = 500;
 
     public KeyguardIndicationTextView(Context context) {
         super(context);
@@ -50,59 +45,19 @@ public class KeyguardIndicationTextView extends TextView {
     }
 
     /**
-     * Changes the text, optionally with an animation.
-     *
-     * @param text The text to show.
-     * @param animate Whether or not the change should be animated.
-     */
-    public void switchIndication(CharSequence text, boolean animate) {
-	if(animate) {
-	    switchIndication(text);
-	} else if(TextUtils.isEmpty(text)) {
-	    setAlpha(0f);
-	} else {
-	    setText(text);
-	    setAlpha(1f); // alpha might already be 1 when we are here, but setting it again won't hurt in that case
-	}
-    }
-
-    /**
      * Changes the text with an animation and makes sure a single indication is shown long enough.
      *
      * @param text The text to show.
      */
     public void switchIndication(CharSequence text) {
-        // TODO: Make sure that we will show one indication long enough.
-        if (TextUtils.isEmpty(text) && getAlpha() == 1f) {
-            animateAlpha(0f, ANIMATION_DURATION, Interpolators.ALPHA_OUT, null);
-        } else if(getAlpha() == 0f) {
-            animateAlpha(1f, ANIMATION_DURATION, Interpolators.ALPHA_IN, null);
-        } else {
-            // Fade out the current indication
-            animateAlpha(0f, ANIMATION_DURATION / 2, Interpolators.ALPHA_OUT, new Runnable() {
-                @Override
-                public void run() {
-                    // Fade in the new indication
-                    setText(text);
-                    animateAlpha(1f, ANIMATION_DURATION / 2, Interpolators.ALPHA_IN, null);
-                }
-            });
-        }
-    }
 
-    private void animateAlpha(float targetAlpha, long duration, Interpolator interpolator, Runnable endAction) {
-        animate()
-        .alpha(targetAlpha)
-        .setDuration(duration)
-        .setInterpolator(interpolator)
-        .withEndAction(new Runnable() {
-            @Override
-            public void run() {
-                if(endAction != null){
-                    endAction.run();
-                }
-            }
-        });
+        // TODO: Animation, make sure that we will show one indication long enough.
+        if (TextUtils.isEmpty(text)) {
+            setVisibility(View.INVISIBLE);
+        } else {
+            setVisibility(View.VISIBLE);
+            setText(text);
+        }
     }
 
     /**
