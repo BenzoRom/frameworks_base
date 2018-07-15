7c770d523535309e43b8b3ff75e2089b144ddbb7
Michael Bestas <mkbestas@lineageos.org>
2018-07-15 22:54:36 +0300

    SystemUI: Add tunables for clock AM/PM style
    Change-Id: I7f27f135ae0d1e731c936792f655c115f107d20c
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -98,5 +98,6 @@ public class SystemSettings {
         Settings.System.ACCELEROMETER_ROTATION_ANGLES,
         Settings.System.VOLUME_KEY_CURSOR_CONTROL,
         Settings.System.STATUS_BAR_CLOCK,
+        Settings.System.STATUS_BAR_AM_PM,
     };
 }
