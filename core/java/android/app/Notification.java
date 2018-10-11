d8ae9908b037eae4f2c11aee095b464f05f75ad7
Adarsh-MR <adarshmr1998@gmail.com>
2018-10-11 12:41:20 +0000

    OMS: Exposures: Squash commits for pie on gerrit
    Author: Adarsh-MR <adarshmr1998@gmail.com>
    
        Themes: Set UserIcons tintMode to SRC_ATOP
    
    Author: Marko Man <darkobas@gmail.com>
    
        Themes: Fix autofill save dialog
    
    Author: ezio84 <brabus84@gmail.com>
    
        Expose autofill save dialog text color
    
    Author: SpiritCroc <dev@spiritcroc.de>
    
        Themes: Add Theme.DeviceDefault.Resolver.Exposed
    
    Author: SpiritCroc <spiritcroc@gmail.com>
    
        Themes: add bool to optimize dynamic notification colors on dark backgrounds
    
    Author: Nicholas Chum <nicholaschum@gmail.com>
    
        Themes: Expose a bool to disable dynamic notification colors
    
    Author: Adarsh-MR <adarshmr1998@gmail.com>
    
        Themes: Expose QS tiles and label colors
    
    Author: Adarsh-MR <adarshmr1998@gmail.com>
    
        Themes: SystemUI: expose screen pinning colors
    
    Author: Ivan Iskandar <iiiiskandar14@gmail.com>
    
        Themes: SystemUI: Expose QS edit item decoration background color
    
    Author: Nicholas Chum <nicholaschum@gmail.com>
    
        Themes: Allow Permission Icons to be fully themed
    
    Author: Nicholas Chum <nicholaschum@gmail.com>
    
        Themes: Allow Immersive cling colors to be fully themed
    
    Author: Dave Kover <dkover@cyngn.com>
    
        Themes: Expose resolver hardcoded colors
    
        Author: Dave Kover <dkover@cyngn.com>
    
            Themes: Expose resolver hardcoded colors
    
        Author: Thyrus11 <thyrus11@gmail.com>
    
            Themes: Make resolver list fully themeable
    
        Author: Nicholas Chum <nicholaschum@gmail.com>
    
            Themes: Allow Resolver List BG to be fully themed
    
    Author: Adarsh-MR <adarshmr1998@gmail.com>
    
        Themes: Expose toast & tooltip text colors for themes
    
    Author: Branden M <wasabi.dev@gmail.com>
    
        Themes: Expose Keyguard affordance circle background
    
    Author: Randall Rushing <randall.rushing@gmail.com>
    
        Themes: Allow volume slider and thumb to work with custom images.
    
    Author: ezio84 <brabus84@gmail.com>
    
        Themes: Expose Screenshot flash and background colors
    
    Author: Bryan Owens <djbryan3540@gmail.com>
    
        Themes: Expose Keyboard Shortcuts Dialog
    
    Author: George G <kreach3r@users.noreply.github.com>
    
        Themes: graphics: ADB "N" icon compatible with OMS7
    
    Author: daveyannihilation <daveyannihilation@hotmail.com>
    
        Themes: SystemUI: Expose switch bar title
    
    commit f8817490a513a3c739416549fbd6291bf056b255
    Author: Dave Kover <dkover@cyngn.com>
    Date:   Thu Apr 14 10:19:13 2016 +0700
    
        Themes: Allow Navbar ripple color to be themed
    
    Change-Id: I8285214532cc6cd60fd516d0b5c9d1aae44489da
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -4433,6 +4433,11 @@ public class Notification implements Parcelable
         private CharSequence processTextSpans(CharSequence text) {
             if (hasForegroundColor()) {
                 return NotificationColorUtil.clearColorSpans(text);
+            } else if (mContext.getResources()
+                .getBoolean(R.bool.config_useDarkBgNotificationIconTextTinting)) {
+                // Some notifications have color spans, assuming a dark background,
+                // so let's remove them
+                return NotificationColorUtil.clearColorSpans(text);
             }
             return text;
         }
@@ -5405,7 +5410,7 @@ public class Notification implements Parcelable
             } else if (isColorized()) {
                 color = getPrimaryTextColor();
             } else {
-                color = resolveContrastColor();
+                color = resolveIconContrastColor();
             }
             if (colorable) {
                 contentView.setDrawableTint(R.id.icon, false, color,
@@ -5425,7 +5430,7 @@ public class Notification implements Parcelable
             if (largeIcon != null && isLegacy()
                     && getColorUtil().isGrayscaleIcon(mContext, largeIcon)) {
                 // resolve color will fall back to the default when legacy
-                contentView.setDrawableTint(R.id.icon, false, resolveContrastColor(),
+                contentView.setDrawableTint(R.id.icon, false, resolveIconContrastColor(),
                         PorterDuff.Mode.SRC_ATOP);
             }
         }
@@ -5436,7 +5441,18 @@ public class Notification implements Parcelable
             }
         }
 
+        int resolveIconContrastColor() {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+                return mContext.getColor(R.color.notification_icon_default_color);
+            } else {
+                return resolveContrastColor();
+            }
+        }
+
         int resolveContrastColor() {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+                return mContext.getColor(R.color.notification_text_default_color);
+            }
             if (mCachedContrastColorIsFor == mN.color && mCachedContrastColor != COLOR_INVALID) {
                 return mCachedContrastColor;
             }
@@ -5448,8 +5464,10 @@ public class Notification implements Parcelable
                 ensureColors();
                 color = NotificationColorUtil.resolveDefaultColor(mContext, background);
             } else {
+                boolean isDark = mInNightMode || mContext.getResources()
+                        .getBoolean(R.bool.config_useDarkBgNotificationIconTextTinting);
                 color = NotificationColorUtil.resolveContrastColor(mContext, mN.color,
-                        background, mInNightMode);
+                        background, isDark);
             }
             if (Color.alpha(color) < 255) {
                 // alpha doesn't go well for color filters, so let's blend it manually
@@ -5474,6 +5492,9 @@ public class Notification implements Parcelable
         }
 
         int resolveAmbientColor() {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+                return mContext.getColor(R.color.notification_ambient_icon_default_color);
+            }
             if (mCachedAmbientColorIsFor == mN.color && mCachedAmbientColorIsFor != COLOR_INVALID) {
                 return mCachedAmbientColor;
             }
