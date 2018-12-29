2d566b07989b27699315a143bdd7ce5735e1e2d4
Wang Han <416810799@qq.com>
2018-12-29 23:33:20 +0800

    ShortcutService: Use ro.benzo.version to signal package scanning
     * Affects system apps scanning.
    
    Change-Id: I5f6d6647929f5b5ae7e820b18e95bf5ed2ec8d1c
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -4790,7 +4790,7 @@ public class ShortcutService extends IShortcutService.Stub {
 
     // Injection point.
     String injectBuildFingerprint() {
-        return Build.FINGERPRINT;
+        return Build.BENZO;
     }
 
     final void wtf(String message) {
