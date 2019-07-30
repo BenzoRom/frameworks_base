syberia-project/platform_frameworks_base

SystemUI: Ambient display music ticker [1/2]
Trigger an Ambient Display notification, even if Ambient
Display is disabled, so we can know playing track title/artist.
This is handy when the player starts playing a new track or even
when we use volume rocker skip track feature when screen is off.
A new track will force an ambient notification but with a clean
layout without clock and other elements, but track info in the middle
of the screen.

Double tapping on track info allows to skip the track (even for
remote Spotify/KdeConnect or other similar apps).

We use provided Doze services to skip this when the proximity
sensor is covered or system Ambient events are pending.

If the feature is disabled, track info is still shown by default
on keyguard and ambient when these modes are called by the system
or the user.

BenzoEdit: Added missing stuff from Pulse commits
Change-Id: I1bf1ad83bce7c5a2a14824ac2993569d05356db8
Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: DennySPB <dennyspb@gmail.com>

core/java/com/android/internal/hardware/AmbientDisplayConfiguration.java
@@ -39,7 +39,8 @@ public class AmbientDisplayConfiguration {
                 || pulseOnPickupEnabled(user)
                 || pulseOnDoubleTapEnabled(user)
                 || pulseOnLongPressEnabled(user)
-                || alwaysOnEnabled(user);
+                || alwaysOnEnabled(user)
+                || pulseOnMedia(user);
     }
 
     public boolean available() {
@@ -49,6 +50,16 @@ public class AmbientDisplayConfiguration {
 
     public boolean pulseOnNotificationEnabled(int user) {
         return boolSettingDefaultOn(Settings.Secure.DOZE_ENABLED, user) && pulseOnNotificationAvailable();
+    }
+
+    public boolean pulseOnMedia(int user) {
+        boolean enabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FORCE_AMBIENT_FOR_MEDIA, 0, user) != 0;
+        return enabled && ambientDisplayAvailable();
+    }
+
+    public boolean canForceDozeNotifications() {
+        return mContext.getResources().getBoolean(R.bool.config_canForceDozeNotifications);
     }
 
     public boolean pulseOnNotificationAvailable() {

BenzoRom/frameworks_base

core/java/com/android/internal/hardware/AmbientDisplayConfiguration.java
@@ -39,6 +39,7 @@ public class AmbientDisplayConfiguration {
                 || pulseOnPickupEnabled(user)
                 || pulseOnDoubleTapEnabled(user)
                 || pulseOnLongPressEnabled(user)
+                || pulseOnMedia(user)
                 || alwaysOnEnabled(user));
     }
 
@@ -49,6 +50,16 @@ public class AmbientDisplayConfiguration {
 
     public boolean pulseOnNotificationEnabled(int user) {
         return boolSettingDefaultOn(Settings.Secure.DOZE_ENABLED, user) && pulseOnNotificationAvailable();
+    }
+
+    public boolean pulseOnMedia(int user) {
+        boolean enabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FORCE_AMBIENT_FOR_MEDIA, 0, user) != 0;
+        return enabled && ambientDisplayAvailable();
+    }
+
+    public boolean canForceDozeNotifications() {
+        return mContext.getResources().getBoolean(R.bool.config_canForceDozeNotifications);
     }
 
     public boolean pulseOnNotificationAvailable() {