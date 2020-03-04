LineageOS/android_frameworks_base

SystemUI: Add reboot fastboot to power menu
Change-Id: I88f00a273f4a0be1355158c58a24db78cb8e684e

packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -137,6 +137,9 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
                 } else if (PowerManager.REBOOT_DOWNLOAD.equals(reason)) {
                     message.setText(
                             com.android.systemui.R.string.global_action_restart_download_progress);
+                } else if (PowerManager.REBOOT_FASTBOOT.equals(reason)) {
+                    message.setText(
+                            com.android.systemui.R.string.global_action_restart_fastboot_progress);
                 }
             } else {
                 message.setText(com.android.systemui.R.string.global_action_restart_progress);

BenzoRom/frameworks_base

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -136,6 +136,9 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
                 } else if (PowerManager.REBOOT_BOOTLOADER.equals(reason)) {
                     message.setText(
                             com.android.systemui.R.string.global_action_reboot_bootloader_progress);
+                } else if (PowerManager.REBOOT_FASTBOOT.equals(reason)) {
+                    message.setText(
+                            com.android.systemui.R.string.global_action_reboot_fastboot_progress);
                 }
             } else {
                 message.setText(com.android.systemui.R.string.global_action_reboot_progress);