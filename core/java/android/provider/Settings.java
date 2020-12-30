a0ca0a51453b0abf7845c83ca5b0308a6c21f1cb
Jason Edson <jaysonedson@gmail.com>
2020-12-29 23:01:00 -0800

    Add touch sensitivity switch from GoogleSettings [2/2]
    For devices that have touch sensitivity (glove_mode) enabled
    in their kernel like the Pixel 4a (5G) and Pixel 5.
    
    Set config_show_touch_sensitivity to true in the device tree
    or vendor overlays to enable the switch.
    
    Change-Id: Icd793fdc291a064826bc6971857ed0f9becd9488
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9235,6 +9235,13 @@ public final class Settings {
         public static final String ADAPTIVE_CONNECTIVITY_ENABLED = "adaptive_connectivity_enabled";
 
         /**
+         * Whether the Touch Sensitivity option is enabled.
+         *
+         * @hide
+         */
+        public static final String TOUCH_SENSITIVITY_ENABLED = "touch_sensitivity_enabled";
+
+        /**
          * Whether to vibrate when quick settings tile is pressed.
          *
          * @hide
