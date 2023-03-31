HurtCopain/derp13_fwb

Add option to scramble pin layout when unlocking (2/2)
Change-Id: Ic168dae91a3280bdeb33281acdcd94edea1375fe
Signed-off-by: Jason Edson <jaysonedson@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -209,5 +209,6 @@ public class SecureSettings {
         Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON,
         Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE,
         Settings.Secure.DOZE_ON_CHARGE,
+        Settings.Secure.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT,
     };
 }

BenzoRom/frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -219,6 +219,7 @@ public class SecureSettings {
         Settings.Secure.BLUETOOTH_LE_BROADCAST_PROGRAM_INFO,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_CODE,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_APP_SOURCE_NAME,
-        Settings.Secure.ADVANCED_REBOOT
+        Settings.Secure.ADVANCED_REBOOT,
+        Settings.Secure.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT
     };
 }
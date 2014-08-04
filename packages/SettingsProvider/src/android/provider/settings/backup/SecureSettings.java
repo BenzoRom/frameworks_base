e0033bc2c03fe05f5ad859f2551b6c1910230526
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Add option to scramble pin layout when unlocking (2/2)
    Change-Id: Ic168dae91a3280bdeb33281acdcd94edea1375fe
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -219,6 +219,7 @@ public class SecureSettings {
         Settings.Secure.BLUETOOTH_LE_BROADCAST_PROGRAM_INFO,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_CODE,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_APP_SOURCE_NAME,
-        Settings.Secure.ADVANCED_REBOOT
+        Settings.Secure.ADVANCED_REBOOT,
+        Settings.Secure.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT
     };
 }
